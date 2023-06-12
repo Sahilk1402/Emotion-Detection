3ce34b31239ccb21de537731ad47d20d9d4fc261
Sahilk1402 <81619744+Sahilk1402@users.noreply.github.com>
2023-06-12 22:51:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import inline as inline
+import matplotlib
+import matplotlib.pyplot as plt
+from glob import glob
+import cv2
+import random
+import os
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense,Input,Dropout,Flatten,Conv2D
